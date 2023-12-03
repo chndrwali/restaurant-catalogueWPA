012ec4a52b76bf5680098ef773b2df3c4f42e897
chndrwali <chndrwali@gmail.com>
2023-12-03 11:09:13 +0700

    change structure code webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,11 @@
-/* eslint-disable prefer-regex-literals */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 const ImageminMozjpeg = require('imagemin-mozjpeg');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -43,7 +43,32 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
   plugins: [
+    new CleanWebpackPlugin(),
+
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/templates/index.html'),
@@ -51,10 +76,30 @@ module.exports = {
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/'),
+          from: path.resolve(__dirname, 'src/public'),
+          to: path.resolve(__dirname, 'dist'),
+          globOptions: {
+          // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
+          //  ignore: ['**/images/**'],
+          },
         },
       ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'candra-food-V2',
+
+          },
+        },
+      ],
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
     }),
     new ImageminWebpackPlugin({
       plugins: [
@@ -63,37 +108,17 @@ module.exports = {
           progressive: true,
         }),
       ],
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+      config: [
         {
-          from: path.resolve(__dirname, 'src/public'),
-          to: path.resolve(__dirname, 'dist'),
-          globOptions: {
-            // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
-            ignore: ['**/images/heros/**'],
-          },
-        },
-      ],
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      openAnalyzer: false,
-    }),
-    new WorkboxWebpackPlugin.GenerateSW({
-      swDest: './sw.bundle.js',
-      runtimeCaching: [
-        {
-          urlPattern: new RegExp('^https://restaurant-api.dicoding.dev/'),
-          handler: 'StaleWhileRevalidate',
+          test: /\.(jpe?g|png)/,
           options: {
-            cacheName: 'jayaapps',
-            cacheableResponse: {
-              statuses: [200],
-            },
+            quality: 50,
           },
         },
       ],
+      overrideExtension: true,
     }),
+
   ],
+
 };
