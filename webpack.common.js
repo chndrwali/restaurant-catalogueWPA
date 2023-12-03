8df2ffb54ffaac4c528bf9ea65621e744f983d90
chndrwali <chndrwali@gmail.com>
2023-12-03 12:46:02 +0700

    undo

--- webpack.common.js
+++ webpack.common.js
@@ -77,7 +77,7 @@ module.exports = {
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public'),
-          to: path.resolve(__dirname, 'dist/public'),
+          to: path.resolve(__dirname, 'dist'),
           globOptions: {
           // CopyWebpackPlugin mengabaikan berkas yang berada di dalam folder images
           //  ignore: ['**/images/**'],
